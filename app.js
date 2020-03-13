e2527d638a32263e3a3f6e36869b0b462b7e449f
Lucas <lucasotavio53@hotmail.com>
2020-03-13 18:43:54 -0300

    smpp class

--- app.js
+++ app.js
@@ -1,39 +1,29 @@
 const {connect} = require("amqplib");
-const {session} = require("src/smpp");
+const {Smpp} = require("./src/smpp");
 
 async function main() {
     try {
+        const smpp = new Smpp();
+        smpp.connect();
         const queue = process.argv.AMQP_QUEUE || "default_queue";
-        session.on("connect", () => {
-            session.bind_transceiver({
-                system_id: process.argv.SMPP_USER,
-                password: process.argv.SMPP_PASSWORD
-            }, async (pdu) => {
-                if(pdu.command_status === 0) {
-                    const amqp_connection = await connect({
-                        protocol: 'amqp',
-                        hostname: process.argv.AMQP_HOST,
-                        port: process.argv.AMQP_PORT || 5672,
-                        username: process.argv.AMQP_USERNAME || 'guest',
-                        password: process.argv.AMQP_PASSWORD || 'guest',
-                        locale: process.argv.AMQP_LOCALE || 'en_US',
-                        frameMax: 0,
-                        heartbeat: process.argv.AMQP_HEARTBEAT || 0,
-                        vhost: process.argv.AMQP_VHOST || '/',
-                    });
-                    const channel = await amqp_connection.createChannel();
-                    if(typeof process.argv.AMQP_PREFETCH != "undefined") {
-                        channel.prefetch(process.argv.AMQP_PREFETCH);
-                    }
-                    channel.assertQueue(queue, {durable: true});
-                    channel.consume(queue, (msg) => {
-                        const data = JSON.parse(msg.content);
-                    });
-                    session.on("deliver_sm", (pdu) => {
-
-                    });
-                }
-            });
+        const amqp_connection = await connect({
+            protocol: 'amqp',
+            hostname: process.argv.AMQP_HOST,
+            port: process.argv.AMQP_PORT || 5672,
+            username: process.argv.AMQP_USERNAME || 'guest',
+            password: process.argv.AMQP_PASSWORD || 'guest',
+            locale: process.argv.AMQP_LOCALE || 'en_US',
+            frameMax: 0,
+            heartbeat: process.argv.AMQP_HEARTBEAT || 0,
+            vhost: process.argv.AMQP_VHOST || '/',
+        });
+        const channel = await amqp_connection.createChannel();
+        if(typeof process.argv.AMQP_PREFETCH != "undefined") {
+            channel.prefetch(process.argv.AMQP_PREFETCH);
+        }
+        channel.assertQueue(queue, {durable: true});
+        channel.consume(queue, (msg) => {
+            const data = JSON.parse(msg.content);
         });
     } catch (e) {
         throw `Something was wrong if connection: ${e.message}`;
@@ -42,5 +32,4 @@ async function main() {
 
 main().catch(reason => {
     console.error(reason);
-    process.exit(0)
 });
--- src/smpp.js
+++ src/smpp.js
@@ -1,21 +1,75 @@
-const smpp = require("smpp");
+const smpp = require("smpp"),
+      iconv = require("iconv-lite")
+      _ = require("lodash");
 
-const session = smpp.connect({
-    url: ``,
-    auto_enquire_link_period: 10000
-});
+delete smpp.encodings.UCS2;
 
-session.on("error", (error) => {
-    console.error(error.message);
-});
+class Smpp {
+    constructor() {
+        this._isConnected = false;
+        this._session = undefined;
+    }
 
-session.on("unknown", (pdu) => {
+    connect() {
+        this._session = smpp.connect({
+            url: ``,
+            auto_enquire_link_period: 10000
+        });
 
-});
+        this._session.on("connect", () => {
+            this._session.bind_transceiver({
+                system_id: process.argv.SMPP_USER,
+                password: process.argv.SMPP_PASSWORD
+            }, async (pdu) => {
+                if (pdu.command_status === 0) {
+                    this._isConnected = true;
+                }
+            });
+        });
 
-session.on("close", () => {
-    console.error("This session is close. Trying to reconnect...");
-    setTimeout(session.connect, 30000);
-});
+        this._session.on("deliver_sm", (pdu) => {
+            if (_.isString(pdu.short_message.message) && pdu.esm_class === 4) {
+                let responseStatus = pdu.short_message.message.replace(/ date/g,"_date").split(" ");
+                let responseStatusObj = {};
+                _.each(responseStatus, (elements) => {
+                    element = elements.split(":");
+                    responseStatusObj[element[0]] = element[1];
+                });
+                responseStatusObj["destination_addr"] = pdu.destination_addr;
+                responseStatusObj["source_addr"] = pdu.source_addr;
+                rabbit.publish("dlr", responseStatusObj);
+                this._session.send(pdu.response());
+            } else {
+                if (pdu.data_coding === 8) {
+                    pdu.short_message.message = iconv.decode(pdu.short_message.message, 'latin1');
+                }
+                if (pdu.short_message.message.indexOf('dlvrd:') === -1) {
+                    rabbit.publish("mo", pdu);
+                    session.send(pdu.response());
+                }
+            }
+        });
 
-exports.session = session;
+        this._session.on("error", (error) => {
+            this._isConnected = false;
+            console.error(error.message);
+            this._session.close();
+        });
+
+        this._session.on("unknown", (pdu) => {
+
+        });
+
+        this._session.on("close", () => {
+            this._isConnected = false;
+            console.error("This this._session is close. Trying to reconnect...");
+            setTimeout(() => { this._session.connect() }, 30000);
+        });
+    }
+
+    isConnected() {
+        return this._isConnected;
+    }
+}
+
+module.exports = Smpp;
