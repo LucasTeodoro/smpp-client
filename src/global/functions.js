411fd7839902379a1e6929a227ba4a4862c6a4cc
Lucas <lucasotavio53@hotmail.com>
2020-03-24 23:41:57 -0300

    sendToQueue

--- src/global/functions.js
+++ src/global/functions.js
@@ -10,8 +10,8 @@ global.nack = (msg) => {
 
 global.publish = (queue, msg) => {
     if(exchange === undefined) {
-        channel.publish(queue, buffer(msg), {persistent: process.argv.AMQP_PERSIST || true});
+        channel.sendToQueue(queue, buffer(msg), {persistent: process.argv.AMQP_PERSIST || true});
     } else {
-        channel.publish(exchange, queue, buffer(msg), {persistent: process.argv.AMQP_PERSIST || true});
+        channel.sendToQueue(exchange, queue, buffer(msg), {persistent: process.argv.AMQP_PERSIST || true});
     }
 };
