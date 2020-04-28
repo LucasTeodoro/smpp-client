816d1ba4137b3ad83da905a314ff591f57ecf084
Lucas Otávio Teodoro Silva <lucasotavio53@hotmail.com>
2020-04-28 18:33:45 -0300

    merge

--- app.js
+++ app.js
@@ -1,46 +1,13 @@
-require("./src/global/functions");
-require("./src/global/variables");
-const Smpp = require("./src/smpp");
-const Amqp = require("./src/connections/rabbit");
-const rules = require("send-rules");
+const express = require("express"),
+    http = require("http"),
+    app = express(),
+    server = http.createServer(app),
+    clients = require("./src/connectores"),
+    port = process.env.SERVER_PORT || 9500;
 
-async function main() {
-    try {
-        const smpp = new Smpp();
-        smpp.connect();
-        const amqp = await new Amqp();
-        global.channel = await amqp.createChannel();
-
-        if(typeof process.argv.AMQP_PREFETCH != "undefined") {
-            channel.prefetch(process.argv.AMQP_PREFETCH);
-        }
-        channel.assertQueue(queue, {durable: process.argv.AMQP_PERSIST || true});
-        channel.assertQueue(sendQueue, {durable: process.argv.AMQP_PERSIST || true});
-        channel.assertQueue(confirmQueue, {durable: process.argv.AMQP_PERSIST || true});
-        channel.assertQueue(receiveQueue, {durable: process.argv.AMQP_PERSIST || true});
-        if(exchange !== undefined) {
-            channel.assertExchange(exchange, "topic", {durable: process.argv.AMQP_PERSIST || true});
-        }
-        channel.consume(queue, async (msg) => {
-            const data = JSON.parse(msg.content);
-            if(smpp.isConnected()) {
-                try {
-                    const {message_id, command_status} = await smpp.send(data.message, data.number);
-                    publish(sendQueue,{message_id: data.message_id, send_message_id: message_id, command_status: command_status});
-                    channel.ack(msg);
-                    return;
-                } catch (e) {
-                    console.log(e.message);
-                }
-            }
-
-            nack(msg);
-        });
-    } catch (e) {
-        throw `Something was wrong if connection: ${e.message}`;
-    }
-}
-
-main().catch(reason => {
-    console.error(reason);
+server.listen(port, async () => {
+    console.log(`> Server listen on port: ${port}`);
+    const manager = await clients();
+    await manager.newConnector("teste1", "fakesmpp", "2775", "user", "pass");
+    await manager.newConnector("teste2", "fakesmpp", "2775", "user", "pass");
 });
--- src/global/variables.js
+++
@@ -1,5 +0,0 @@
-global.queue = process.argv.AMQP_QUEUE || "default_queue";
-global.sendQueue = `send.${queue}`;
-global.confirmQueue = `confirm.${queue}`;
-global.receiveQueue = `receive.${queue}`;
-global.exchange = process.argv.AMQP_EXCHANGE;
